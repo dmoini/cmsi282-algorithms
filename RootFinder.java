ca2d0a6c1271e8891472dcb80650b1894a1d187e
dmoini <donnym99@gmail.com>
2018-01-20 20:03:24 -0800

    Cleaned up code.

--- RootFinder.java
+++ RootFinder.java
@@ -1,6 +1,5 @@
 public class RootFinder {
     private static double root(double x, double epsilon, int exponent) {
-        //(Math.pow(x, 2)).setPrecision(3) == (x + (x * epsilon)).setPrecision(3) || Math.pow(x, 2).setPrecision(3) == (x - (x * epsilon)).setPrecision(3)
         if (x < 0) {
             throw new IllegalArgumentException("Please input a positive number.");
         }
@@ -12,7 +11,7 @@ public class RootFinder {
         double upperBound = toPrecision(x + (x * epsilon), 3);
         if (x > 0 && x < 1) {
             root = (x + 1) / 2;
-        } else { // x > 1
+        } else {  // x > 1 or x == 0
             root = x / 2;
         }
         while (toPrecision(Math.pow(root, exponent), 3) < lowerBound || toPrecision(Math.pow(root, exponent), 3) > upperBound) {
@@ -28,9 +27,5 @@ public class RootFinder {
     private static double toPrecision(double number, int places) {
         int multiplier = (int) Math.pow(10, places);
         return (double) ((long) (number * multiplier)) / multiplier;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(root(0, 0.001, 12));
     }
 }
