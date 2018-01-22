d01a38d8918989a0d583c56ccc1d5c5bf6d74c3a
dmoini <donnym99@gmail.com>
2018-01-22 13:38:32 -0800

    Updated parameters.

--- RootFinder.java
+++ RootFinder.java
@@ -1,31 +1,35 @@
 public class RootFinder {
-    private static double root(double x, double epsilon, int exponent) {
+    private static double sqrt(double x, double epsilon) {
         if (x < 0) {
             throw new IllegalArgumentException("Please input a positive number.");
         }
         if (x == 1) {
             return 1;
         }
-        double root;
+        double y;
         double lowerBound = toPrecision(x - (x * epsilon), 3);
         double upperBound = toPrecision(x + (x * epsilon), 3);
         if (x > 0 && x < 1) {
-            root = (x + 1) / 2;
+            y = (x + 1) / 2;
         } else {  // x > 1 or x == 0
-            root = x / 2;
+            y = x / 2;
         }
-        while (toPrecision(Math.pow(root, exponent), 3) < lowerBound || toPrecision(Math.pow(root, exponent), 3) > upperBound) {
-            if (Math.pow(root, exponent) > x) {
-                root /= 2;
-            } else if (Math.pow(root, exponent) < x) {
-                root += root / 2;
+        while (toPrecision(Math.pow(y, 2), 3) < lowerBound || toPrecision(Math.pow(y, 2), 3) > upperBound) {
+            if (Math.pow(y, 2) > x) {
+                y /= 2;
+            } else if (Math.pow(y, 2) < x) {
+                y += y / 2;
             }
         }
-        return toPrecision(root, 3);
+        return toPrecision(y, 3);
     }
 
     private static double toPrecision(double number, int places) {
         int multiplier = (int) Math.pow(10, places);
         return (double) ((long) (number * multiplier)) / multiplier;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sqrt(12, 0.001));
     }
 }
