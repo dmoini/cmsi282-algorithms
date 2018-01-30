f6a6716deea13f72090869cbec71fc9a0023f074
dmoini <donnym99@gmail.com>
2018-01-29 17:16:47 -0800

    Create PowersAndPolynomials.java
    Java class containing methods such as finding any integer x to any integer power n, evaluating any polynomial, evaluating the greatest common denominator between two integers, and evaluating the least common multiple between two integers.

---
+++ PowersAndPolynomials.java
@@ -0,0 +1,31 @@
+public class PowersAndPolynomials {
+    public static int xToPowerN(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n % 2 == 0) {
+            return xToPowerN(x, n / 2) * xToPowerN(x, n / 2);
+        } else {
+            return x * xToPowerN(x, n / 2) * xToPowerN(x, n / 2);
+        }
+    }
+
+    public static int polynomialEvaluation(int[] polynomial, int x) {
+        int result = polynomial[0];
+        for (int i = 1; i < polynomial.length; i++) {
+            result = result * x + polynomial[i];
+        }
+        return result;
+    }
+
+    public static long gcd(int x, int y) {
+        if (x % y == 0) {
+            return y;
+        }
+        return gcd(y, x % y);
+    }
+
+    public static long lcm(int x, int y) {
+        return (x * y) / gcd(x, y);
+    }
+}
